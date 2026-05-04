a794c70cfc23ac111b35c5e284d05b2c9f070179
Shams Kabir <shamskbr27@gmail.com>
2026-05-04 11:30:49 +0600

    Refactor shams_gcc.ps1 for clarity and structure

--- shams_gcc.ps1
+++ shams_gcc.ps1
@@ -46,12 +46,12 @@ while ($job.State -eq 'Running') {
 Receive-Job $job -ErrorAction Stop | Out-Null
 Remove-Job $job
 
-Write-Host "`n`r  Done!                              " -ForegroundColor Green
+Write-Host "`r  `nDone!                              " -ForegroundColor Green
 
 Write-Host "Extracting..." -ForegroundColor Cyan
 Expand-Archive -Path $ZipFile -DestinationPath $InstallDir -Force
 
-Write-Host "`n`r  Done!                              " -ForegroundColor Green
+Write-Host "`r  `nDone!                              " -ForegroundColor Green
 
 if (Test-Path $ZipFile) { Remove-Item $ZipFile -Force }
 
